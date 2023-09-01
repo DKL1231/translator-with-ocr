a84a9e8fae4633d2a3b687f97b81a6d52b7c6e6b
DKL <95444292+DKL1231@users.noreply.github.com>
2023-09-01 20:03:10 +0900

    Create google-trans.py and rename papagotrans.py

---
+++ google-trans.py
@@ -0,0 +1,25 @@
+import googletrans
+import CustomDict
+
+class googletranslator:
+    def __init__(self):
+        self.translator = googletrans.Translator()
+        self.src = 'ja' # default src is japanese
+        self.dest = 'ko' # default dest is korean
+        self.CustomDict = CustomDict.CustomDict()
+    
+    def translate(self, text):
+        text = self.CustomDict.sentenceProcessing(text)
+        print(text)
+        result = self.translator.translate(text, src=self.src, dest=self.dest)
+        print(result)
+    
+    def setLanguage(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+
+if __name__ == "__main__":
+    translator = googletranslator()
+    print(translator.translate("天ちゃんがめぐるちゃんに向けて言った。"))
+    
