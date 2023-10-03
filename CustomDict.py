52bfc1b3b6082026a89228f1fc65eaf0d27071f6
dkl1231 <akhack1231@skuniv.ac.kr>
2023-10-03 22:41:17 +0900

    Update Customdict
    dict add button added

--- CustomDict.py
+++ CustomDict.py
@@ -1,34 +1,35 @@
 class CustomDict:
+    CustomDict = {}
     def __init__(self):
         self.filename = "CustomDict/CustomDict.txt"
         self.encoding = 'UTF-8'
         self.Dictfile = None
-        self.CustomDict = {}
         self.updateDict()
     
     def appendDict(self, origin, trans):
         self.Dictfile = open(self.filename, "a", encoding=self.encoding)
-        self.Dictfile.write(f"\n{origin}\t{trans}")
+        self.Dictfile.write(f"{origin}\t{trans}\n")
         self.Dictfile.close()    
         
         self.updateDict()
     
     def updateDict(self):
         self.Dictfile = open(self.filename, "r", encoding=self.encoding)
-        self.CustomDict = {}
+        CustomDict.CustomDict = {}
         
         for sentence in self.Dictfile.readlines():
+            if sentence[:2] == "//":
+                continue
             origin, trans = sentence.split("\t")
             trans = trans[:-1]
             
-            if not origin in self.CustomDict:
-                self.CustomDict[origin] = trans
-            #print(origin, self.CustomDict[origin])
+            if not origin in CustomDict.CustomDict:
+                CustomDict.CustomDict[origin] = trans
+            #print(origin, CustomDict.CustomDict[origin])
 
-    
     def sentenceProcessing(self, sentence):
         newsentence = sentence
-        for key, value in self.CustomDict.items():
+        for key, value in CustomDict.CustomDict.items():
             newsentence = newsentence.replace(key, value)
         return newsentence
 
--- customdictwindows.py
+++ customdictwindows.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
+import CustomDict
 
 class customwindows:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("CustomDict")
-        self.root.geometry("500x300")
+        self.root.geometry("500x400")
         self.filename = "CustomDict/CustomDict.txt"
         f = open(self.filename, "r", encoding = "UTF-8")
         
@@ -21,12 +22,46 @@ class customwindows:
             print(origin, trans[:-1])
             self.c_dict[origin] = trans[:-1]
         
+        f.close()
         
-        
-        # 원문 - 번역문 테이블 생성
         tableLabel = tk.Label(self.root, text="CustomDict")
         tableLabel.pack(pady=(10, 10))
         
+        addwordframe = tk.LabelFrame(self.root, text="단어 추가/변경")
+        addwordframe.pack(pady=(0,10))
+        
+        origintext = tk.Label(addwordframe, text="원문 :")
+        origintext.grid(row=0, column=0, padx=(5, 5), pady=(10, 10))
+        self.origininput = tk.Text(addwordframe, width=10, height=1)
+        self.origininput.grid(row=0, column=1, padx=(0, 5), pady=(10, 10))
+        
+        transtext = tk.Label(addwordframe, text="번역문 :")
+        transtext.grid(row=0, column=2, padx=(0, 5), pady=(10, 10))
+        self.transinput = tk.Text(addwordframe, width=10, height=1)
+        self.transinput.grid(row=0, column=3, padx=(0, 5), pady=(10, 10))
+        
+        def addfunction():
+            tmpdict = CustomDict.CustomDict()
+            o_input, t_input = self.origininput.get("1.0", tk.END)[:-1], self.transinput.get("1.0", tk.END)[:-1]
+            tmpdict.appendDict(o_input, t_input)
+            self.dicttable.insert("", "end", text="", values=(o_input, t_input), iid = o_input)
+        
+        self.addbutton = tk.Button(addwordframe, text="추가", command=addfunction)
+        self.addbutton.grid(row=0, column=4, padx=(0, 5), pady=(10, 10))
+        
+        def changefunction():
+            pass
+        
+        self.changebutton = tk.Button(addwordframe, text="변경")
+        self.changebutton.grid(row=0, column=5, padx=(0, 5), pady=(10, 10))
+        
+        def deletefunction():
+            pass
+        
+        self.deletebutton = tk.Button(addwordframe, text="삭제")
+        self.deletebutton.grid(row=0, column=6, padx=(0, 5), pady=(10, 10))
+        
+        # 원문 - 번역문 테이블 생성
         self.dicttable = ttk.Treeview(self.root, columns=["Origin Text", "Translated Text"], displaycolumns=["Origin Text", "Translated Text"])
         self.dicttable.pack()
         
