57316993c03e01d9f4c3b4131f0497e35e9eab4d
DKL <95444292+DKL1231@users.noreply.github.com>
2023-10-16 14:52:42 +0900

    Update CustomDict
    Add TTS-CustomDict algorithm

--- CustomDict.py
+++ CustomDict.py
@@ -1,23 +1,25 @@
+import googletrans
 class CustomDict:
     CustomDict = {}
     ProcessDict = {}
     process_num = 1
     process_alpha = 0
+    translator = googletrans.Translator()
     def __init__(self):
         self.filename = "CustomDict/CustomDict.txt"
         self.encoding = 'UTF-8'
         self.Dictfile = None
         self.updateDict()
     
-    def appendDict(self, origin, trans):
+    def appendDict(self, origin, trans, type):
         self.Dictfile = open(self.filename, "a", encoding=self.encoding)
-        self.Dictfile.write(f"{origin}\t{trans}\n")
+        self.Dictfile.write(f"{origin}\t{trans}\t{type}\n")
         self.Dictfile.close()    
         
         self.updateDict()
     
-    def removeDict(self, origin, trans):
-        s_delete = f"{origin}\t{trans}\n"
+    def removeDict(self, origin, trans, type):
+        s_delete = f"{origin}\t{trans}\t{type}\n"
         new_t = ""
         with open(self.filename, "r", encoding=self.encoding) as f:
             while True:
@@ -34,9 +36,9 @@ class CustomDict:
             f.write(new_t)
         self.updateDict()
 
-    def changeDict(self, origin, trans, c_origin, c_trans):
-        s_delete = f"{origin}\t{trans}\n"
-        s_insert = f"{c_origin}\t{c_trans}\n"
+    def changeDict(self, origin, trans, type, c_origin, c_trans, c_type):
+        s_delete = f"{origin}\t{trans}\t{type}\n"
+        s_insert = f"{c_origin}\t{c_trans}\t{c_type}\n"
         new_t = ""
         with open(self.filename, "r", encoding=self.encoding) as f:
             while True:
@@ -62,18 +64,18 @@ class CustomDict:
                 continue
             if sentence[:2] == "//":
                 continue
-            origin, trans = sentence.split("\t")
-            trans = trans[:-1]
+            origin, trans, type = sentence.split("\t")
+            type = type[:-1]
             
             if not origin in CustomDict.CustomDict:
-                CustomDict.CustomDict[origin] = trans
+                CustomDict.CustomDict[origin] = (trans, type)
 
     def sentenceProcessing(self, sentence):
         newsentence = sentence
         for key, value in CustomDict.CustomDict.items():
             process_code = f'_{chr(65+CustomDict.process_alpha)}{CustomDict.process_num}'
             newsentence = newsentence.replace(key, process_code)
-            CustomDict.ProcessDict[process_code] = value
+            CustomDict.ProcessDict[process_code] = value[0]
             CustomDict.process_num += 1
             if CustomDict.process_num == 10:
                 CustomDict.process_num = 1
@@ -85,11 +87,25 @@ class CustomDict:
         for key, value in CustomDict.ProcessDict.items():
             newsentence = newsentence.replace(key, value)
         CustomDict.ProcessDict = {}
+        CustomDict.process_alpha = 0
+        CustomDict.process_num = 0
         return newsentence
+
+    def sentenceProcessing_TTS(self, sentence):
+        newsenetence = sentence
+        for key, value in CustomDict.CustomDict.items():
+            if key in newsenetence and value[1] == '원음 표기':
+                newvalue = CustomDict.translator.translate(value[0])
+                newvalue = newvalue.extra_data['origin_pronunciation']
+                newvalue = CustomDict.translator.translate(newvalue, src='en', dest='ja').text
+                newsenetence = newsenetence.replace(key, newvalue)
+        return newsenetence
 
 if __name__ == "__main__":
     customdict = CustomDict()
-    process1 = customdict.sentenceProcessing("天ちゃんがめぐるちゃんに向けて言った。")
+    process0 = customdict.sentenceProcessing_TTS("黒坂 優香子さんの別名はゆかるんです。")
+    print(process0)
+    process1 = customdict.sentenceProcessing("黒坂 優香子さんの別名はゆかるんです。")
     print(process1)
     process2 = customdict.sentenceProcessing_reverse(process1)
     print(process2)
--- customdictwindows.py
+++ customdictwindows.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox
 from tkinter import ttk
 import CustomDict
 
@@ -6,7 +7,7 @@ class customwindows:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("CustomDict")
-        self.root.geometry("500x400")
+        self.root.geometry("600x400")
         self.filename = "CustomDict/CustomDict.txt"
         f = open(self.filename, "r", encoding = "UTF-8")
         
@@ -20,9 +21,9 @@ class customwindows:
                 continue
             if line[:2] == "//": # 주석부분은 제외
                 continue
-            origin, trans = line.split('\t')
-            print(origin, trans[:-1])
-            self.c_dict[origin] = trans[:-1]
+            origin, trans, type = line.split('\t')
+            print(origin, trans, type)
+            self.c_dict[origin] = (trans, type[:-1])
         
         f.close()
         
@@ -42,71 +43,94 @@ class customwindows:
         self.transinput = tk.Text(addwordframe, width=10, height=1)
         self.transinput.grid(row=0, column=3, padx=(0, 5), pady=(10, 10))
         
+        tagtext = tk.Label(addwordframe, text="유형 : ")
+        tagtext.grid(row=0, column=4, padx=(0, 5), pady=(10, 10))
+        values = ['의미 표기', '원음 표기']
+        self.taginput = ttk.Combobox(addwordframe, height=3, width=4, values=values)
+        self.taginput.grid(row=0, column=5, padx=(0, 5), pady=(10, 10))
+        self.taginput.set('선택')
+        
         def addfunction():
             tmpdict = CustomDict.CustomDict()
             o_input, t_input = self.origininput.get("1.0", tk.END)[:-1], self.transinput.get("1.0", tk.END)[:-1]
-            tmpdict.appendDict(o_input, t_input)
-            self.dicttable.insert("", "end", text="", values=(o_input, t_input), iid = o_input)
+            type_input = self.taginput.get()
+            if type_input == '선택' or o_input=='' or t_input=='':
+                tkinter.messagebox.showwarning(title="입력 에러", message="모든 항목을 정확히 입력해 주세요")
+                return
+            try:
+                self.dicttable.insert("", "end", text="", values=(o_input, t_input, type_input), iid = o_input)
+            except:
+                tkinter.messagebox.showwarning(title="입력 에러", message="해당 원문은 이미 존재합니다.")
+                return
+            tmpdict.appendDict(o_input, t_input, type_input)
         
         self.addbutton = tk.Button(addwordframe, text="추가", command=addfunction)
-        self.addbutton.grid(row=0, column=4, padx=(0, 5), pady=(10, 10))
+        self.addbutton.grid(row=0, column=6, padx=(0, 5), pady=(10, 10))
         
         def changefunction():
             try:
                 curItem = self.dicttable.focus()
             except:
                 return
-            o_input, t_input = self.dicttable.item(curItem)['values']
+            o_input, t_input, type_input = self.dicttable.item(curItem)['values']
             co_input, ct_input = self.origininput.get("1.0", tk.END)[:-1], self.transinput.get("1.0", tk.END)[:-1]
+            ctype_input = self.taginput.get()
             tmpDict = CustomDict.CustomDict()
-            tmpDict.changeDict(o_input, t_input, co_input, ct_input)
-            self.dicttable.item(curItem, text="", values=(co_input, ct_input))
+            tmpDict.changeDict(o_input, t_input, type_input, co_input, ct_input, ctype_input)
+            self.dicttable.item(curItem, text="", values=(co_input, ct_input, ctype_input))
         
         self.changebutton = tk.Button(addwordframe, text="변경", command=changefunction)
-        self.changebutton.grid(row=0, column=5, padx=(0, 5), pady=(10, 10))
+        self.changebutton.grid(row=0, column=7, padx=(0, 5), pady=(10, 10))
         
         def deletefunction():
             try:
                 curItem = self.dicttable.focus()
             except:
                 return
-            o_input, t_input = self.dicttable.item(curItem)['values']
+            o_input, t_input, type_input = self.dicttable.item(curItem)['values']
             tmpDict = CustomDict.CustomDict()
-            tmpDict.removeDict(o_input, t_input)
+            tmpDict.removeDict(o_input, t_input, type_input)
             selected_item = self.dicttable.selection()[0] ## get selected item
             self.dicttable.delete(selected_item)
             self.origininput.delete("1.0", "end")
             self.transinput.delete("1.0", "end")
         
         self.deletebutton = tk.Button(addwordframe, text="삭제", command=deletefunction)
-        self.deletebutton.grid(row=0, column=6, padx=(0, 5), pady=(10, 10))
+        self.deletebutton.grid(row=0, column=8, padx=(0, 5), pady=(10, 10))
         
         def dicttableclick(event):
             item = self.dicttable.identify('item', event.x, event.y)
             try:
-                o_input, t_input = self.dicttable.set(item).values()
+                o_input, t_input, type_input = self.dicttable.set(item).values()
             except:
                 return
             self.origininput.delete("1.0", "end")
             self.origininput.insert("1.0", o_input)
             self.transinput.delete("1.0","end")
             self.transinput.insert("1.0", t_input)
+            self.taginput.set(type_input)
         
         # 원문 - 번역문 테이블 생성
-        self.dicttable = ttk.Treeview(self.root, columns=["Origin Text", "Translated Text"], displaycolumns=["Origin Text", "Translated Text"])
+        self.dicttable = ttk.Treeview(self.root, columns=["Origin Text", "Translated Text", "Type"], displaycolumns=["Origin Text", "Translated Text", "Type"])
         self.dicttable.bind("<Button-1>", dicttableclick)
         self.dicttable.pack()
         
-        self.dicttable.column("Origin Text", width="200", anchor="center")
+        self.dicttable.column("Origin Text", width="192", anchor="center")
         self.dicttable.heading("Origin Text", text="Origin Text")
         
-        self.dicttable.column("Translated Text", width="200", anchor="center")
+        self.dicttable.column("Translated Text", width="192", anchor="center")
         self.dicttable.heading("Translated Text", text="Translated Text")
+        
+        self.dicttable.column("Type", width="100", anchor="center")
+        self.dicttable.heading("Type", text="Type")
         
         self.dicttable["show"] = "headings"
         
-        for origin, trans in self.c_dict.items():
-            self.dicttable.insert("", "end", text="", values=(origin, trans), iid = origin)
+        for origin, (trans, type) in self.c_dict.items():
+            try:
+                self.dicttable.insert("", "end", text="", values=(origin, trans, type), iid = origin)
+            except:
+                pass
         
 
 if __name__ == "__main__":
